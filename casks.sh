c0b2905bf16deaa58e1094995fd9f0c02bcf95a5
MozzarellaM <4561733+MozzarellaM@users.noreply.github.com>
2020-02-09 20:03:56 -0500

    Update dotfiles, add iTerm config

--- casks.sh
+++ casks.sh
@@ -3,43 +3,9 @@
 # Google Chrome
 brew cask install google-chrome
 
-# Google Backup and Sync
-brew cask install google-backup-and-sync
 
 # Google Play Music Desktop Player
 brew cask install marshallofsound-google-play-music-player
-
-# Hyper Terminal
-brew cask install hyper
-
-# Discord
-brew cask install discord
-
-# Private Internet Access VPN
-brew cask install private-internet-access
-
-# Fonts
-brew tap homebrew/cask-fonts
-brew cask install font-fira-code
-brew cask install font-cascadia
-brew cask install font-fira-code
-brew cask install font-hack
-brew cask install font-hasklig
-brew cask install font-monoid
-brew cask install font-roboto
-
-# The Unarchiver
-brew cask install the-unarchiver
-
-# Games
-brew cask install minecraft
-brew cask install retroarch
-brew cask install openmw
-brew cask install steam
-brew cask install mgba
-
-# Aerial Screensaver
-brew cask install aerial
 
 # Visual Studio Code
 brew cask install visual-studio-code
@@ -52,3 +18,47 @@ brew cask install iterm2
 
 # Install iTerm 2 Shell Integration 
 curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
+
+# The Unarchiver
+brew cask install the-unarchiver
+
+# Private Internet Access VPN
+brew cask install private-internet-access
+
+# Google Backup and Sync
+brew cask install google-backup-and-sync
+
+# VLC
+brew cask install vlc
+
+# Hyper Terminal
+brew cask install hyper
+
+# Discord
+brew cask install discord
+
+# Productivity
+brew cask install typora
+brew cask install qbittorent
+
+
+# Games
+brew cask install minecraft
+brew cask install retroarch
+brew cask install openmw
+brew cask install steam
+brew cask install mgba
+
+# Fonts
+brew tap homebrew/cask-fonts
+brew cask install font-fira-code
+brew cask install font-cascadia
+brew cask install font-fira-code
+brew cask install font-hack
+brew cask install font-hasklig
+brew cask install font-monoid
+brew cask install font-roboto
+
+
+# Aerial Screensaver
+brew cask install aerial
