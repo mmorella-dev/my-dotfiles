86fe95df74284714d574d250eb5e3c22015b1915
M Morella <4561733+MozzarellaM@users.noreply.github.com>
2020-04-30 20:30:26 -0400

    Update fish configuration

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,23 @@ end
 
 # remove startup message
 set fish_greeting
+
+# use zsh ls colors
+set -Ux LSCOLORS Gxfxcxdxbxegedabagacad
+
+# use zsh-syntax-highlighting color palette
+set fish_color_command green
+set fish_color_error red --bold
+set fish_color_autosuggestion brblack
+set fish_color_comment brblack
+set fish_color_escape magenta
+set fish_color_quote yellow
+set fish_color_normal white
+set fish_color_end cyan
+set fish_color_param white
+set fish_color_operator blue
+set fish_color_redirection white
+set fish_color_match --background brblue
+
+# homebrew sbin
+set -g fish_user_paths "/usr/local/sbin/" $fish_user_paths
