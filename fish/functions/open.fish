4453c4e3c5b451888e040ec51bbcee3d37959365
M Morella <4561733+MozzarellaM@users.noreply.github.com>
2020-04-30 20:29:51 -0400

    Fix cross-platform 'open' function

--- fish/functions/open.fish
+++ fish/functions/open.fish
@@ -1,13 +1,13 @@
-# normalize "open" across platforms
 function open
-  if [ ! (uname -s) = 'Darwin' ]
-    if grep -q Microsoft /proc/version
-      # Ubuntu on Windows using the Linux subsystem
-      alias open='explorer.exe'
+    if [ ! (uname -s) = 'Darwin' ]
+        if grep -q Microsoft /proc/version
+            # Ubuntu on Windows using the Linux subsystem
+            alias open='explorer.exe'
+        else
+            alias open='xdg-open'
+        end
     else
-      alias open='xdg-open'
+        alias open='/usr/bin/open'
     end
-  else
-    alias open='/usr/bin/open'
-  end
+    open $argv
 end
