0970d9db1c15c89623cecc32af3f88f14451b7b0
M Morella <4561733+MozzarellaM@users.noreply.github.com>
2020-05-10 21:44:22 -0400

    Update link.sh

--- link.sh
+++ link.sh
@@ -27,8 +27,10 @@ make_links() {
   local dest_dir=$2
   for file in $source_files; do
     local path_of_dest_file=$(realpath -s $dest_dir/$(basename $file))
-    if [ ! $file -ef $path_of_dest_file ]; then
+    if [ ! $file -ef $(readlink $path_of_dest_file) ]; then
+      printf "${BOLD}${BLUE}"
       ln -sviF $file $dest_dir
+      printf "${NOCOL}"
     else
       printf "${BOLD}${CYAN}$(realpath --relative-base . $file) is already linked.${NOCOL}\n"
     fi
