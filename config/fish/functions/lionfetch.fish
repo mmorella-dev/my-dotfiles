7fdab99f1dcd56af9960855d3f98399098481246
M Morella <4561733+MozzarellaM@users.noreply.github.com>
2020-05-29 17:30:00 -0400

    Add fish aliases for neofetch styles

--- config/fish/functions/lionfetch.fish
+++ config/fish/functions/lionfetch.fish
@@ -1,4 +1,4 @@
 # Defined in - @ line 1
-function lionfetch --wraps='neofetch --iterm2 ~/Desktop/raion2.png --colors 4 4 8 7 3' --wraps='neofetch --iterm2 ~/Scripts/raion2.png --colors 4 4 8 7 3' --wraps='neofetch --iterm2 ~/Scripts/raion2.png --size 25% --colors 4 4 8 7 3 3 7 7' --description 'alias lionfetch=neofetch --iterm2 ~/Scripts/raion2.png --size 25% --colors 4 4 8 7 3 3 7 7'
-  neofetch --iterm2 ~/Scripts/raion2.png --size 25% --colors 4 4 8 7 3 3 7 7 $argv;
+function lionfetch --wraps='neofetch --iterm2 ~/Desktop/raion2.png --colors 4 4 8 7 3' --wraps='neofetch --iterm2 ~/Scripts/raion2.png --colors 4 4 8 7 3' --wraps='neofetch --iterm2 ~/Scripts/raion2.png --size 25% --colors 4 4 8 7 3 3 7 7' --wraps='neofetch --iterm2 ~/.config/neofetch/lion.png --colors 4 4 4 3 6 7' --description 'alias lionfetch=neofetch --iterm2 ~/.config/neofetch/lion.png --colors 4 4 4 3 6 7'
+  neofetch --iterm2 ~/.config/neofetch/lion.png --colors 4 4 4 3 6 7 $argv;
 end
