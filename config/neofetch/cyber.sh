d428c560b07902492360e2859f7fb91d9400fa63
M Morella <4561733+MozzarellaM@users.noreply.github.com>
2020-05-31 02:12:02 -0400

    Add bash scripts for neofetch themes

---
+++ config/neofetch/cyber.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+neofetch --colors 9 9 7 13 9 5 --iterm2 "$HOME/.config/neofetch/sunset.png"
---
+++ config/neofetch/lion.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+neofetch --iterm2 "$HOME/.config/neofetch/lion.png" --colors 4 4 4 3 6 7
