434a5eb820aff259d6d27287869ddbbceb7c262b
M Morella <4561733+MozzarellaM@users.noreply.github.com>
2020-06-03 20:42:28 -0400

    Update neofetch config

--- config/neofetch/cyber.sh
+++ config/neofetch/cyber.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-neofetch --colors 9 9 7 13 9 5 --iterm2 "$HOME/.config/neofetch/sunset.png"
+neofetch --colors 9 9 7 13 9 5 --source "$HOME/.config/neofetch/sunset.png" --ascii_colors 9 13 14 9 15 13 14 $argv
--- config/neofetch/lion.sh
+++ config/neofetch/lion.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-neofetch --iterm2 "$HOME/.config/neofetch/lion.png" --colors 4 4 4 3 6 7
+neofetch --iterm2 "$HOME/.config/neofetch/lion.png" --colors 4 4 4 3 6 7 --ascii_colors 3 4 7 4 3 4 $argv
