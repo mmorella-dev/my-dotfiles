0dfeba949bfbde66673962bb481916c2db3b3f66
M Morella <70173714+morellam-dev@users.noreply.github.com>
2020-08-27 15:03:53 -0400

    Rewrote link script. Much simpler, relies on lndir

---
+++ link.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+command -v lndir >/dev/null || echo "This script needs xutils-dev or imake to be installed."
+
+# remove DS store files.
+find . -type f -name "*.DS_Store" -ls -delete
+
+# deep shadow directory of all dotfiles into $HOME
+lndir $(realpath ./home/) $HOME/
